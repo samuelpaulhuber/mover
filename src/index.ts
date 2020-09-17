f386ea95db7765f6a13ca5b20a05d7c38a55dd19
Sam Huber <sam.huber@deluxe.com>
2020-09-17 11:10:26 -0400

    adjust timing for testing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import RandomMovement from './mouse/RandomMovement';
 
 const randomMovement = new RandomMovement();
-randomMovement.moveBackForthUpDown(100, 60, 10);
+randomMovement.moveBackForthUpDown(100, 5400, 10);
